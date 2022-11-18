ea82a2ecceac0deb4c37399e4e24f26cba152fb9
Robson Cavalcanti Lins Júnior <robson.2021104910@unicap.br>
2022-11-18 18:48:21 +0000

    Adjusting Sintático

--- CompiladorR/Sintatico.java
+++ CompiladorR/Sintatico.java
@@ -18,6 +18,7 @@ public class Sintatico {
                 if (currToken.getLexema().equals("(")) {
                     currToken = scanner.nextToken();
                     if (currToken.getLexema().equals(")")) {
+                        currToken = scanner.nextToken();
                         bloco();
                     } else {
                         throw new RuntimeException("Erro na iniciação do programa Esperado: )"); 
@@ -34,12 +35,21 @@ public class Sintatico {
     }
 
     public void bloco() {
-        currToken = scanner.nextToken();
         if (currToken.getLexema().equals("{")) {
-            decl_var();
-            comando();
             currToken = scanner.nextToken();
+            while (currToken.getTipo() != Token.TIPO_FIM_CODIGO && !currToken.getLexema().equals("}")) {
+                if (currToken.getLexema().equals("int")|| 
+                currToken.getLexema().equals("float") || 
+                currToken.getLexema().equals("char")) 
+                {
+                   decl_var(); 
+                }
+                else{
+                   comando(); 
+                }
+            }
             if (currToken.getLexema().equals("}")) {
+                currToken = scanner.nextToken();
             }else{
                 throw new RuntimeException("Bloco mal formatado");
             } 
@@ -48,69 +58,86 @@ public class Sintatico {
         }
     }
 
-    private void comando() {
-        currToken = scanner.nextToken();
+    public void comando() {
         if(currToken.getLexema().equals("if")){
+            condicional();
+        }
+        else if(currToken.getLexema().equals("while")){
+            iteração();
+        }
+        else if(currToken.getTipo() == 3 || currToken.getLexema().equals("{")) {
+            comandoBasico();
+        }
+    }
+
+    public void comandoBasico() {
+        if (currToken.getTipo() == 3) {
+            atribuição();
+        } else if(currToken.getLexema().equals("{")) {
+            bloco();
+        }else{
+            throw new RuntimeException("Comando básico inválido");
+        }
+    }
+
+    public void atribuição() {
+        if (currToken.getTipo() == 3) {
             currToken = scanner.nextToken();
-            if (currToken.getLexema().equals("(")) {
+            if (currToken.getLexema().equals("=")) {
+                currToken = scanner.nextToken();
+                expr_arit();
+                if (currToken.getLexema().equals(";")) {
+                    currToken = scanner.nextToken();
+                } else{
+                    throw new RuntimeException("Atribuicao inválida"); 
+                }
+            } else {
+                throw new RuntimeException("Atribuicao inválida"); 
+            }
+        }else{
+            throw new RuntimeException("Atribuicao inválida"); 
+        }
+    }
+
+    public void iteração() {
+        currToken = scanner.nextToken();
+        if (currToken.getLexema().equals("(")) {
+            currToken = scanner.nextToken();
+            expr_relacional();
+            if (currToken.getLexema().equals(")")) {
+                currToken = scanner.nextToken();
+            }else{
+                throw new RuntimeException("Iteração mal formatada");
+            }
+        }else{
+            throw new RuntimeException("Iteração mal formatada");
+        }
+    }
+
+    public void condicional() {
+        currToken = scanner.nextToken();
+        if (currToken.getLexema().equals("(")) {
+            currToken = scanner.nextToken();
                 expr_relacional();
                 if (currToken.getLexema().equals(")")) {
+                    currToken = scanner.nextToken();
                     comando();
-                    //currToken = scanner.nextToken();
-                    //if (currToken.getLexema().equals("else")) {
-                       // comando();
-                    //}
+                    if (currToken.getLexema().equals("else")) {
+                        currToken = scanner.nextToken();
+                        comando();
+                    }
                 } else {
                     throw new RuntimeException("Condicional mal formatado");
                 }
             }else{
                 throw new RuntimeException("Condicional mal formatado");
             }
-        }
-        else if(currToken.getLexema().equals("while")){
-            currToken = scanner.nextToken();
-            if (currToken.getLexema().equals("(")) {
-                expr_relacional();
-                if (currToken.getLexema().equals(")")) {
-                    comando();
-                } else {
-                    throw new RuntimeException("Iteração mal formatado");
-                }
-            } else {
-                throw new RuntimeException("Iteração mal formatado");
-            }
-        }
-        else if(currToken.getTipo() == 3) {
-            currToken = scanner.nextToken();
-            if (currToken.getLexema().equals("=")) {
-                expr_arit();
-                currToken = scanner.nextToken();
-                if (currToken.getLexema().equals(";")) {
-                    
-                } else {
-                    throw new RuntimeException("Atribuição mal formatado");
-                }
-            } else {
-                throw new RuntimeException("Atribuição mal formatado");
-            }
-        }
-        else if(currToken.getLexema().equals("{")){
-            decl_var();
-            comando();
-            currToken = scanner.nextToken();
-            if (currToken.getLexema().equals("}")) {
-            }else{
-                throw new RuntimeException("Bloco mal formatado");
-            } 
-        }
-        else{
-            throw new RuntimeException("Comando mal formatado");
-        }
     }
 
     public void expr_relacional() {
         expr_arit();
         if (currToken.getTipo() == 4) {
+            currToken = scanner.nextToken();
             expr_arit();
         }else{
             throw new RuntimeException("Expr relacional mal formatada");
@@ -123,70 +150,60 @@ public class Sintatico {
     }
 
     public void EA() {
-        if (currToken.getLexema().equals("+")) {
+        if (currToken.getLexema().equals("+") || currToken.getLexema().equals("-")) {
+            currToken = scanner.nextToken();
             termo();
             EA();
-        }
-        if (currToken.getLexema().equals("-")) {
-            termo();
-            EA();
-        }
-        else{
-            return;
         }
     }
 
     public void termo() {
         fator();
-        currToken = scanner.nextToken();
         TL();
     }
 
     public void TL() {
-        if (currToken.getLexema().equals("*")) {
+        if (currToken.getLexema().equals("*") || currToken.getLexema().equals("/")) {
+            currToken = scanner.nextToken();
             fator();
             TL();
-        }
-        if (currToken.getLexema().equals("/")) {
-            fator();
-            TL();
-        }
-        else{
         }
     }
 
     public void fator() {
-        currToken = scanner.nextToken();
         if (currToken.getLexema().equals("(")) {
-            expr_relacional();
             currToken = scanner.nextToken();
+            expr_relacional();
             if (currToken.getLexema().equals(")")) {
+                currToken = scanner.nextToken();
             }else{
                 throw new RuntimeException("Fator mal formatado");
             }
         }else if (currToken.getTipo() == 0 || currToken.getTipo() == 1 || currToken.getTipo() == 2 || currToken.getTipo() == 3) {
-            
+            currToken = scanner.nextToken();
         }else{
             throw new RuntimeException("Fator mal formatado");
         }
     }
 
     public void decl_var() {
-        currToken = scanner.nextToken();
-        if (currToken.getLexema().equals("int")| currToken.getLexema().equals("float") | currToken.getLexema().equals("char")) {
-            currToken = scanner.nextToken();
-            if (currToken.getTipo() == 3) {
-                currToken = scanner.nextToken();
+        if ((currToken.getLexema().equals("int")||
+        currToken.getLexema().equals("float") ||
+        currToken.getLexema().equals("char") )) {
+            currToken = scanner.nextToken();   
+            if (currToken.getTipo() == Token.TIPO_IDENTIFICADOR) {
+                currToken = scanner.nextToken();  
                 if (currToken.getLexema().equals(";")) {
+                    currToken = scanner.nextToken();  
+                }else{
+                    throw new RuntimeException("Declaração de variável mal formatada");
                 }
-                else{
-                    throw new RuntimeException("Declaração de variavel mal formatada");
-                }
-            } else {
-                throw new RuntimeException("Declaração de variavel mal formatada");
+            } 
+            else{
+                throw new RuntimeException("Declaração de variável mal formatada");
             }
         }else{
-            throw new RuntimeException("Declaração de variavel mal formatada");
+            throw new RuntimeException("Declaração de variável mal formatada");
         }
     }
 }
